0ef74670089556fab38d7c08f746d648a929c415
wjkuhao <wjkuhao@163.com>
2019-01-07 16:46:44 +0800

    这是IDEA的提交测试

---
+++ git/src/test/TestGit.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
